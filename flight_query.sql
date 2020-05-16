85142c2481af2b7e931e69eb56176eb134657894
settenson23 <41304919+settenson23@users.noreply.github.com>
2020-05-15 22:14:37 -0700

    Delete flight_query.sql

--- flight_query.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM airline_codes;
-SELECT * FROM flight_datas;
--- Join tables on airline_id
-SELECT airline_codes.airline_id, airline_codes.airline_name, flight_datas.airline_id, flight_datas.flight_number, flight_datas.departure_airport, flight_datas.destination_airport,	flight_datas.departure_time, flight_datas.departure_delay_15_mins, flight_datas.day_of_the_month, flight_datas.day_of_the_week
-FROM airline_codes
-JOIN flight_datas
-ON airline_codes.airline_id = flight_datas.airline_id;
